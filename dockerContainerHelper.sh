c4c64949df77c95018db5c0369adf865507ad00b
David <daccomazzo@gradguard.com>
2021-10-26 16:14:58 -0700

    add some basic sanity checks so the code is less garbage

--- dockerContainerHelper.sh
+++ dockerContainerHelper.sh
@@ -8,8 +8,36 @@ function dockerExecInContainer {
     docker exec -it $( getContainerFromDockerCompose ) $1
 }
 
+function help {
+    printf "Usage: `container-exec bash` or `container exec 'composer install'`"
+}
 
-printf "You passed in command \033[38;5;233m\033[48;5;50m$1\033[0m to container \033[38;5;233m\033[48;5;50m$( getContainerFromDockerCompose )\033[0m\n"
-dockerExecInContainer $1
 
+function main {
+    # help
+    while getopts "help" OPTION
+    do
+        help
+        return 0
+    done
+
+    # validate params
+    if [ "$#" -ne 1 ]; then
+        echo "Only pass in one argument. Longer commands can be passed in within single quotes."
+        return 1
+    fi
+
+    # ensure there's an actual docker-compose file to use
+    if [ ! -f docker-compose.yml ]; then
+        printf "No docker-compose.yml file found in directory."
+        return 1
+    fi
+
+    # do the stuff
+    printf "You passed in command \033[38;5;233m\033[48;5;50m$1\033[0m to container \033[38;5;233m\033[48;5;50m$( getContainerFromDockerCompose )\033[0m\n"
+
+    dockerExecInContainer $1
+}
+
+main $1
 
