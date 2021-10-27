b1e4466468a8bd1fd2db6197d941f4c631f149b3
David <daccomazzo@gradguard.com>
2021-10-27 08:14:00 -0700

    fixing bugs

--- dockerContainerHelper.sh
+++ dockerContainerHelper.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
 
+function newline {
+    printf "\n"
+}
+
 function getContainerFromDockerCompose {
     cat docker-compose.yml | grep -i container_name | awk '{print $2}'
 }
 
 function dockerExecInContainer {
-    docker exec -it $( getContainerFromDockerCompose ) $1
+    printf "You passed in command \033[38;5;233m\033[48;5;50m$@\033[0m to container \033[38;5;233m\033[48;5;50m$( getContainerFromDockerCompose )\033[0m\n"
+    printf "docker exec -it $( getContainerFromDockerCompose ) $@"
+    newline
+    docker exec -it $( getContainerFromDockerCompose ) $@
 }
 
 function help {
     printf "Usage: `container-exec bash` or `container exec 'composer install'`\n"
 }
 
-function validateParams {
-    if [ "$#" -ne 1 ]; then
-        printf "Only pass in one argument. Longer commands can be passed in within single quotes.\n"
-        invalid='yes'
-        return 1
-    fi
-
-}
-
-
 function main {
+    echo "$@"
     # help
     while getopts "help" OPTION
     do
         help
         return 0
     done
-
-    # validate params
-    if [ "$#" -ne 1 ]; then
-        printf "Only pass in one argument. Longer commands can be passed in within single quotes.\n"
-        return 1
-    fi
 
     # ensure there's an actual docker-compose file to use
     if [ ! -f docker-compose.yml ]; then
@@ -43,17 +35,7 @@ function main {
     fi
 
     # do the stuff
-    printf "You passed in command \033[38;5;233m\033[48;5;50m$1\033[0m to container \033[38;5;233m\033[48;5;50m$( getContainerFromDockerCompose )\033[0m\n"
-
-    dockerExecInContainer $1
+    dockerExecInContainer "$@"
 }
 
-
-validateParams
-
-# ewwwwww
-if [ "$invalid" == 'yes' ]; then
-    exit
-fi
-
-main $1
+main "$@"
